8c51137c13617892244998e032dd9b1e290c8037
scsketch <sc.sketch@gmail.com>
2025-05-27 02:28:08 -0700

    adding swagger log when server starts

--- src/server.ts
+++ src/server.ts
@@ -9,4 +9,7 @@ const HOST = '0.0.0.0';
 
 app.listen(PORT, HOST, () => {
   console.log(`Server running on http://${HOST}:${PORT}`);
+  if (process.env.NODE_ENV === 'development') {
+    console.log(`Swagger running on http://${HOST}:${PORT}/api-docs`);
+  }
 });
